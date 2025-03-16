e7893195e6205c2f6e5728e987745dfdfc1ec37e
vinod-o <vinodo3735@gmail.com>
2025-03-16 14:41:07 +0530

    Add files via upload

---
+++ src/main/java/springbootapp/spring_app/SpringAppApplication.java
@@ -0,0 +1,13 @@
+package springbootapp.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+}
